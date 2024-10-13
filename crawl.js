c0a0f31ca800a676df578bebf3f510c6e5e3948d
AhmedMaged <osans026@gmail.com>
2024-10-14 00:55:57 +0300

    added the ability the extract urls from html document , and unit testing to ensure the validation of the url being extracted

--- crawl.js
+++ crawl.js
@@ -1,3 +1,23 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  let domObj = new JSDOM(htmlBody);
+  let links = domObj.window.document.links;
+  let extractedURLs = [];
+  for (const link of links) {
+    let fullURL;
+    if (link.href.startsWith("/")) fullURL = `${baseURL}${link.href}`;
+    else fullURL = link.href;
+    try {
+      let urlObj = new URL(fullURL);
+      extractedURLs.push(urlObj.href);
+    } catch (err) {
+      console.log(`error occured! ${err.message}`);
+    }
+  }
+  return extractedURLs;
+}
+
 function normalizeURLs(StringURL) {
   let url = new URL(StringURL);
   let normalizedURL = `${url.hostname}${url.pathname}`;
@@ -8,4 +28,5 @@ function normalizeURLs(StringURL) {
 
 module.exports = {
   normalizeURLs,
+  getURLsFromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,6 @@
 const { normalizeURLs } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
+const { getURLsFromHTML } = require("./crawl.js");
 
 test("normalizeURLs strip protocols", () => {
   const input = "https://youtube.com/path/test";
@@ -40,5 +41,61 @@ test("normalizeURLs strip the port number", () => {
   const input = "https://developer.mozilla.org:443/en-US/./blog/";
   const actual = normalizeURLs(input);
   const expected = "developer.mozilla.org/en-US/blog";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute URL extraction", () => {
+  const inputHtmlBody = `
+<html>
+  <body>
+    <a href="https://youtube.com/">youtube</a>
+  </body>
+</html>`;
+  const inputBaseUrl = "https://youtube.com";
+  const actual = getURLsFromHTML(inputHtmlBody,inputBaseUrl);
+  const expected = ["https://youtube.com/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative URL extraction", () => {
+  const inputHtmlBody = `
+<html>
+  <body>
+    <a href="/watch/">youtube watch</a>
+  </body>
+</html>`;
+  const inputBaseUrl = "https://youtube.com";
+  const actual = getURLsFromHTML(inputHtmlBody,inputBaseUrl);
+  const expected = ["https://youtube.com/watch/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML multiple URLs extraction", () => {
+  const inputHtmlBody = `
+<html>
+  <body>
+    <a href="/watch/">youtube watch</a>
+    <a href="https://youtube.com/">youtube</a>
+    <a href="https://github.com/">github</a>
+  </body>
+</html>`;
+  const inputBaseUrl = "https://youtube.com";
+  const actual = getURLsFromHTML(inputHtmlBody,inputBaseUrl);
+  const expected = ["https://youtube.com/watch/","https://youtube.com/","https://github.com/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid URL catch", () => {
+  const inputHtmlBody = `
+<html>
+  <body>
+    <a href="/watch/">youtube watch</a>
+    <a href="invalid">invalid</a>
+    <a href="https://github.com/">github</a>
+  </body>
+</html>`;
+  const inputBaseUrl = "https://youtube.com";
+  const actual = getURLsFromHTML(inputHtmlBody,inputBaseUrl);
+  const expected = ["https://youtube.com/watch/","https://github.com/"];
   expect(actual).toEqual(expected);
 });
