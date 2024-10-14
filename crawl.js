10a9904b3297b2558388efd85c42548218e1dcd0
AhmedMaged <osans026@gmail.com>
2024-10-15 00:41:25 +0300

    crawl a single page with effectively handling fetching and type errors

--- crawl.js
+++ crawl.js
@@ -1,5 +1,22 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(url) {
+  try{
+    const resp = await fetch(url);
+    if(resp.status>399){
+      console.log(`couldn't retrieve the web page due to error with status code : ${resp.status}`)
+      
+    }else if(!resp.headers.get('content-type').includes("text/html")){
+      console.log(`didn't receive an HTML document , instead got ${resp.headers.get('content-type')} type of data`)
+    }else{
+      const content=await resp.text()
+      console.log(content)
+    }
+  }catch(err){
+    console.log(`error! ${err.message}`)
+  }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
   let domObj = new JSDOM(htmlBody);
   let links = domObj.window.document.links;
@@ -29,4 +46,5 @@ function normalizeURLs(StringURL) {
 module.exports = {
   normalizeURLs,
   getURLsFromHTML,
+  crawlPage,
 };
