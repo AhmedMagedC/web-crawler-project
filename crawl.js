01ee182610bfbbb81e3c956229a8f27e1a21b4a8
AhmedMaged <osans026@gmail.com>
2024-10-17 13:43:06 +0300

    recursively crawling the pages

--- crawl.js
+++ crawl.js
@@ -1,20 +1,42 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(url) {
-  try{
-    const resp = await fetch(url);
-    if(resp.status>399){
-      console.log(`couldn't retrieve the web page due to error with status code : ${resp.status}`)
-      
-    }else if(!resp.headers.get('content-type').includes("text/html")){
-      console.log(`didn't receive an HTML document , instead got ${resp.headers.get('content-type')} type of data`)
-    }else{
-      const content=await resp.text()
-      console.log(content)
+async function crawlPage(baseUrl, currentUrl, pages) {
+  const baseURLObj = new URL(baseUrl);
+  const currentUrlObj = new URL(currentUrl);
+  if (baseURLObj.hostname !== currentUrlObj.hostname) return pages;
+
+  pages[normalizeURLs(currentUrl)] =
+    pages[normalizeURLs(currentUrl)] === undefined
+      ? 1
+      : pages[normalizeURLs(currentUrl)] + 1;
+
+  if (pages[normalizeURLs(currentUrl)] > 1) return pages;
+
+  console.log(`currently crawling : ${currentUrl}`);
+  try {
+    const resp = await fetch(currentUrl);
+    if (resp.status > 399) {
+      console.log(
+        `couldn't retrieve the web page due to error with status code : ${resp.status}`
+      );
+    } else if (!resp.headers.get("content-type").includes("text/html")) {
+      console.log(
+        `didn't receive an HTML document , instead got ${resp.headers.get(
+          "content-type"
+        )} type of data`
+      );
+    } else {
+      const content = await resp.text();
+      const extractedURLs = getURLsFromHTML(content, baseUrl);
+
+      for (const url of extractedURLs) {
+        pages = await crawlPage(baseUrl, url, pages);
+      }
     }
-  }catch(err){
-    console.log(`error! ${err.message}`)
+  } catch (err) {
+    console.log(`error! ${err.message}`);
   }
+  return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
@@ -29,7 +51,7 @@ function getURLsFromHTML(htmlBody, baseURL) {
       let urlObj = new URL(fullURL);
       extractedURLs.push(urlObj.href);
     } catch (err) {
-      console.log(`error occured! ${err.message}`);
+      console.log(`error occured! ${err.message} : ${fullURL}`);
     }
   }
   return extractedURLs;
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const { crawlPage } = require("./crawl.js");
 
-function main() {
+async function main() {
   if (process.argv.length === 3) {
     console.log(`started crawling...`);
-    crawlPage(process.argv[2])
+    const pages = await crawlPage(process.argv[2], process.argv[2], {});
+    for (const page of Object.entries(pages)) console.log(page);
   } else {
     console.log(
       "invalid command argument! you should write 'npm start website-url'"
